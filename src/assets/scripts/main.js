7891ff51a9ccf541ba1850627e7975e7ae115045
Manuel Jesus de la Calle Brihuega <mdelacalleb@uoc.edu>
2024-03-15 14:22:43 +0100

    Validado HTML

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,13 @@
  */
 
 +( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth' // Desplazamiento suave
+      });
+    });
+  });
 } )();
